8c9d9222831b692c1468fe0c3e20ff1b66184019
Sergio Garcia <segarciat314@gmail.com>
2022-12-16 18:35:50 -0500

    Button with animal type populates results with cards

--- client/js/search.js
+++ client/js/search.js
@@ -8,27 +8,102 @@ const ANIMALS_URI = "/animals";
 const ANIMAL_TYPES_URI = "/types";
 const ANIMAL_BREEDS_URI = "/breeds";
 
-const searchForm = document.getElementById("searchForm");
+const ANIMAL_TYPE_ATTRIB = "data-animal-type";
+const SEARCH_RESULTS_ID = "searchResults";
 
-searchForm.addEventListener("submit", handleSearchSubmit);
+window.addEventListener("load", async () => {
+  const data = await getAnimalTypes();
+  displayTypeButtons(data.types);
+});
 
-async function handleSearchSubmit(e) {
-  e.preventDefault();
-  const input = e.target.elements.searchInput.value;
-  // const animals = await getAnimals();
-  // const dogs = await getAnimals({ type: "Dogs" });
-  // const animal = await getAnimalById(59179795);
-  // const types = await getAnimalTypes();
-  // const type = await getSingleAnimalType("Dog");
-  // const breed = await getAnimalBreeds("Dog");
-  console.log(breed);
+function displayTypeButtons(types) {
+  const animalTypes = document.getElementById("animalTypes");
+  console.log(types);
+  types.forEach((type) => {
+    const {
+      name,
+      _links: { self },
+    } = type;
+    // const animalType = self.href.split("/").pop();
+    const col = document.createElement("div");
+    col.classList = "column is-one-quarter";
+    col.innerHTML = `
+    <div class="card">
+      <a ${ANIMAL_TYPE_ATTRIB}="${name}" class="button is-fullwidth is-white">
+        <div class="card-content">
+          <div class="content">${name}</div>
+        </div>
+      </a>
+    </div>
+    `;
+    col.querySelector("a").addEventListener("click", displayResults);
+    animalTypes.append(col);
+  });
+}
+
+async function displayResults(e) {
+  // Highlight button of selected animal.
+  const anchor = e.target.closest(`[${ANIMAL_TYPE_ATTRIB}]`);
+  document.querySelectorAll("#animalTypes a").forEach((a) => {
+    a.classList.remove("is-primary");
+    a.classList.add("is-white");
+  });
+  anchor.classList.add("is-primary");
+
+  // Get animals of depending on button's animal type.
+  const animalType = anchor.getAttribute(ANIMAL_TYPE_ATTRIB);
+  console.log(animalType);
+  const data = await getAnimals({ type: animalType });
+
+  // Display the animals.
+  clearSearchResults();
+  const searchResults = document.getElementById(SEARCH_RESULTS_ID);
+  data.animals.forEach((animal) => {
+    const col = createAnimalCard(animal);
+    searchResults.append(col);
+  });
+}
+
+function createAnimalCard(animal) {
+  {
+    const { name, age, gender, url, id, primary_photo_cropped } = animal;
+    const col = document.createElement("col");
+    const imageSrc = primary_photo_cropped
+      ? `src="${primary_photo_cropped.small}"`
+      : "";
+    col.classList =
+      "column is-one-quarter-tablet is-flex is-justify-content-center";
+    col.innerHTML = `
+  <div class="card is-flex-grow-1">
+    <a href="#" target="_blank" rel="noopener">
+      <div class="card-image">
+        <img
+          style="width: 100%; object-fit: cover; aspect-ratio: 1/1"
+          ${imageSrc}
+          alt="${name}"
+        />
+      </div>
+    </a>
+    <div class="card-content">
+      <div class="content">${name} ${gender} ${age}</div>
+    </div>
+  </div>
+  `;
+    return col;
+  }
+}
+
+function clearSearchResults() {
+  const searchResults = document.getElementById(SEARCH_RESULTS_ID);
+  while (searchResults.firstElementChild)
+    searchResults.firstElementChild.remove();
 }
 
 //
 async function getAnimals(params) {
   // Prepare URL
   const url = new URL(`${PETFINDER_URL}${ANIMALS_URI}`);
-  url.params = new URLSearchParams(params);
+  url.search = new URLSearchParams(params);
 
   // Prepare headers
   const headers = await getAuthHeaders();
