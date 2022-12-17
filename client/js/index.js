a1a19cf958899b5572c906a30f78345267107438
ddjefferson <ddjefferson@yahoo.com>
2022-12-17 13:32:19 -0800

    updated index.js

---
+++ client/js/index.js
@@ -0,0 +1,8 @@
+// mobile menu
+//from DOM
+const burgerIcon = document.querySelector("#burger");
+const navbarMenu = document.querySelector("#nav-links");
+
+burgerIcon.addEventListener("click", () => {
+  navbarMenu.classList.toggle("is-active");
+});
