fd8d98254907ebf6147c2021b4984b908541042b
Sergio Garcia <segarciat314@gmail.com>
2022-12-20 18:47:51 -0500

    Fixed the flip cards

--- client/js/faq.js
+++ client/js/faq.js
@@ -1,21 +1,9 @@
 //looking for inner card
-const card = document.querySelector(".card__inner");
+const cards = document.querySelectorAll(".flip-card__inner");
 // listening for click
 // toggle when is flipped
-card.addEventListener("click", function (e) {
-  card.classList.toggle("is-flipped");
-});
-
-const card2 = document.querySelector(".card2__inner");
-// listening for click
-// toggle when is flipped
-card2.addEventListener("click", function (e) {
-  card2.classList.toggle("is-flipped");
-});
-
-const card3 = document.querySelector(".card3__inner");
-// listening for click
-// toggle when is flipped
-card3.addEventListener("click", function (e) {
-  card3.classList.toggle("is-flipped");
+cards.forEach((card) => {
+  card.addEventListener("click", function (e) {
+    card.classList.toggle("is-flipped");
+  });
 });
